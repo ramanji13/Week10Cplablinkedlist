f113a9b3c4e6890a9df1638b0c58bf7671b96acf
ramanji13 <venkataramanjireddy13@gmail.com>
2024-11-26 22:01:47 +0530

    Length of a linked list

---
+++ Length.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+int data;
+struct node* next;
+};
+int main()
+{
+struct node *head=NULL,*newnode,*temp,*pnode;
+int i,n,pos,count=0;
+printf("enter a value for how many numbers you want to store");
+scanf("%d",&n);
+for(i=1;i<=n;i++){
+newnode=(struct node*)malloc(sizeof(struct node));
+printf("enter a value");
+scanf("%d",&newnode->data);
+newnode->next=0;
+if(head==NULL) head =temp = newnode;
+else{
+temp->next=newnode;
+temp=newnode;
+}
+}
+i=1;
+temp=head;
+while(temp!=0){
+count++;
+printf("%d\n",temp->data);
+temp=temp->next;
+}printf("length is %d\n",count);
+}
